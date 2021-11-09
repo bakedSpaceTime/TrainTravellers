0b82cb34c1f43ddd36c8dfcd71ae2e347ca205fb
Tushya Iyer <tushyais@gmail.com>
2021-11-09 10:05:06 -0800

    fix whitespace

--- Dashboard-UI/src/components/EndpointAudit.js
+++ Dashboard-UI/src/components/EndpointAudit.js
@@ -2,40 +2,40 @@ import React, { useEffect, useState } from 'react'
 import '../App.css';
 
 export default function EndpointAudit(props) {
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [log, setLog] = useState(null);
-    const [error, setError] = useState(null)
-	const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [log, setLog] = useState(null);
+  const [error, setError] = useState(null)
 
-    const getAudit = () => {
-        // fetch(`http://localhost:9010/${props.endpoint}?index=${rand_val}`)
-        fetch(`http://kafka-ti-acit3855.eastus2.cloudapp.azure.com:9010/${props.endpoint}?index=${rand_val}`)
-            .then(res => res.json())
-            .then((result)=>{
-				console.log("Received Audit Results for " + props.endpoint)
-                setLog(result);
-                setIsLoaded(true);
-            },(error) =>{
-                setError(error)
-                setIsLoaded(true);
-            })
-    }
-	useEffect(() => {
-		const interval = setInterval(() => getAudit(), 4000); // Update every 4 seconds
-		return() => clearInterval(interval);
-    }, [getAudit]);
+  const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
 
-    if (error){
-        return (<div className={"error"}>Error found when fetching from API</div>)
-    } else if (isLoaded === false){
-        return(<div>Loading...</div>)
-    } else if (isLoaded === true){
+  const getAudit = () => {
+    // fetch(`http://localhost:9010/${props.endpoint}?index=${rand_val}`)
+    fetch(`http://kafka-ti-acit3855.eastus2.cloudapp.azure.com:9010/${props.endpoint}?index=${rand_val}`)
+      .then(res => res.json())
+      .then((result)=>{
+        console.log("Received Audit Results for " + props.endpoint)
+        setLog(result);
+        setIsLoaded(true);
+      },(error) =>{
+        setError(error)
+        setIsLoaded(true);
+      })
+  }
+  useEffect(() => {
+    const interval = setInterval(() => getAudit(), 4000); // Update every 4 seconds
+    return() => clearInterval(interval);
+  }, [getAudit]);
 
-        return (
-            <div>
-                <h3>{props.endpoint}-{rand_val}</h3>
-                {JSON.stringify(log)}
-            </div>
-        )
-    }
+  if (error){
+    return (<div className={"error"}>Error found when fetching from API</div>)
+  } else if (isLoaded === false){
+    return(<div>Loading...</div>)
+  } else if (isLoaded === true){
+    return (
+      <div>
+      <h3>{props.endpoint}-{rand_val}</h3>
+      {JSON.stringify(log)}
+      </div>
+    )
+  }
 }
