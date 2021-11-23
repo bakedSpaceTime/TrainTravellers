863f1073368ec35bccd42e6b3048c20867d9c9de
Tushya Iyer <tushyais@gmail.com>
2021-11-23 09:32:31 -0800

    add condition for enabling CORS

--- Audit/app.py
+++ Audit/app.py
@@ -96,8 +96,10 @@ logger.info(f"Log Conf File: {log_conf_file}")
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yml", base_path="/audit", strict_validation=True, validate_responses=True)
-CORS(app.app)
-app.app.config['CORS_HEADERS'] = 'Content-Type'
+
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
+    CORS(app.app)
+    app.app.config['CORS_HEADERS'] = 'Content-Type'
 
 if __name__ == "__main__":
     app.run(port=8110, debug=False)
--- Processing/app.py
+++ Processing/app.py
@@ -125,8 +125,10 @@ logger.info(f"Log Conf File: {log_conf_file}")
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yml", base_path="/processing", strict_validation=True, validate_responses=True)
-CORS(app.app)
-app.app.config['CORS_HEADERS'] = 'Content-Type'
+
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
+    CORS(app.app)
+    app.app.config['CORS_HEADERS'] = 'Content-Type'
 
 if __name__ == "__main__":
     init_scheduler()
