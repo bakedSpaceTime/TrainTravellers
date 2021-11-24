07688e3c3cab4234a1d950b36029ac79b219bdcf
Tushya Iyer <tushyais@gmail.com>
2021-11-24 12:54:57 -0800

    remove test logs

--- Storage/app.py
+++ Storage/app.py
@@ -213,8 +213,6 @@ logger.info(f"Connecting to DB. Hostname: {app_config['datastore']['hostname']},
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yml", base_path="/storage", strict_validation=True, validate_responses=True)
 
-logger.info("test log")
-logger.info("test log2")
 
 if __name__ == "__main__":
 
