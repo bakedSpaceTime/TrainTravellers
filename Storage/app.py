b050626bd6b1e37b8f102c500e8d3ac8327c15a0
Tushya Iyer <tushyais@gmail.com>
2021-11-30 17:25:25 -0800

    revert storage scaling settings

--- Storage/app.py
+++ Storage/app.py
@@ -191,15 +191,15 @@ def process_messages():
     # Create a consume on a consumer group, that only reads new messages
     # (uncommitted messages) when the service re-starts (i.e., it doesn't
     # read all the old messages from the history in the message queue).
-    # consumer = topic.get_simple_consumer(consumer_group=b'event',
-    #                                      reset_offset_on_start=False,
-    #                                      auto_offset_reset=OffsetType.LATEST
-    #                                      )
-    consumer = topic.get_balanced_consumer(consumer_group=b'event_group',
-                                    reset_offset_on_start=False,
-                                    zookeeper_connect='zookeeper:2181',
-                                    auto_commit_enable=True,
-                                    auto_commit_interval_ms=100)
+    consumer = topic.get_simple_consumer(consumer_group=b'event',
+                                         reset_offset_on_start=False,
+                                         auto_offset_reset=OffsetType.LATEST
+                                         )
+    # consumer = topic.get_balanced_consumer(consumer_group=b'event_group',
+    #                                 reset_offset_on_start=False,
+    #                                 zookeeper_connect='zookeeper:2181',
+    #                                 auto_commit_enable=True,
+    #                                 auto_commit_interval_ms=100)
 
     # This is blocking - it will wait for a new message
     for msg in consumer:
